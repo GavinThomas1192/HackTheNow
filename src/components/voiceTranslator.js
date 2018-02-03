291309d14a26e705a022fc653d1ae3f051e50f68
GavinThomas1192 <gthomas1192@gmail.com>
2018-02-03 11:22:21 -0800

    progress

--- src/components/voiceTranslator.js
+++ src/components/voiceTranslator.js
@@ -16,6 +16,9 @@ import { ReactMic } from 'react-mic';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 const context = new AudioContext();
+let microphone;
+let analyser;
+let javascriptNode;
 
 const socket = openSocket('http://localhost:8000');
 
@@ -30,7 +33,8 @@ class VoiceTranslator extends React.Component {
             record: false,
             recordedBlobURL: '',
             command: 'stop', 
-            returnedAudioRecordingBinaryFile: 'Nothing yet.',
+            returnedAudioRecordingBinaryFile: [],
+            toggleVolumeReader: false,
 
         }
     }
@@ -41,11 +45,19 @@ class VoiceTranslator extends React.Component {
         socket.on('timer', timestamp => this.setState({ timestamp }));
         socket.on('azureAuth', accessToken => this.setState({ accessToken }));
         socket.on('translationReturned', translation => this.setState({ translation }));
-        socket.on('returnedAudioTranslaton', returnedAudioRecordingBinaryFile => this.setState({returnedAudioRecordingBinaryFile}));
+        socket.on('returnedAudioTranslaton', returnedAudioRecordingBinaryFile => this.handleIncomingBuffers(returnedAudioRecordingBinaryFile));
     }
 
   
+    handleIncomingBuffers = (returnedAudioRecordingBinaryFile) => {
+        console.log(returnedAudioRecordingBinaryFile)
+        let playbackFile = []
 
+        playbackFile.push(returnedAudioRecordingBinaryFile)
+
+        this.setState({returnedAudioRecordingBinaryFile: playbackFile})
+
+    }
     handleGetTranslation = () => {
         socket.emit('translate', `${__AZURE_CLIENT_SECRET__}`)
     }
@@ -53,39 +65,71 @@ class VoiceTranslator extends React.Component {
     componentDidUpdate() {
         console.log('UPDATED', this.state)
 
+        {!this.state.returnedAudioRecordingBinaryFile ? undefined :
 
-        
-        
-    }
+        this.state.returnedAudioRecordingBinaryFile.map(ele => { 
 
-    handlePlaybackTransaltion = () => {
-        context.decodeAudioData(this.state.returnedAudioRecordingBinaryFile, (buffer) => {
+            context.decodeAudioData(ele, (buffer) => {
             const source = context.createBufferSource();
             source.buffer = buffer;
             source.connect(context.destination);
-            // source.loop = loop;
             source.start(0);
-            // source.onended = onended;
-            // this.playbackNode = source;
-            // resolve(source);
+            
           });
+        })
+        }
 
+        
+        
     }
 
-    // play = (audioBuffer) => {
-    //     var source = context.createBufferSource();
-    //     source.buffer = audioBuffer;
-    //     source.connect( context.destination );
-    //     source.start(0);
+    analyzeVolume = () => {
+     
+                navigator.getUserMedia({
+                audio: true
+                },
+                function(stream) {
+                    // stream.getAudioTracks().forEach(function(track){track.stop();})
+                analyser = context.createAnalyser();
+                microphone = context.createMediaStreamSource(stream);
+                javascriptNode = context.createScriptProcessor(2048, 1, 1);
 
-    // }
-    
-    
-    
-    
-    
+                analyser.smoothingTimeConstant = 0.8;
+                analyser.fftSize = 1024;
+
+                microphone.connect(analyser);
+                analyser.connect(javascriptNode);
+                javascriptNode.connect(context.destination);
+
+                javascriptNode.onaudioprocess = function() {
+                    var array = new Uint8Array(analyser.frequencyBinCount);
+                    analyser.getByteFrequencyData(array);
+                    var values = 0;
+
+                    var length = array.length;
+                    for (var i = 0; i < length; i++) {
+                        values += (array[i]);
+                    }
+
+                    var average = values / length;
+
+                     console.log(Math.round(average - 40));
+                }
+
+        
+                },
+                function(err) {
+                console.log("The following error occured: " + err.name)
+                })
+            
+    }
+
+  
+
     
     onChange = (AudioRecorderChangeEvent) => {
+        microphone.disconnect(analyser)
+        javascriptNode.onaudioprocess = null;
         socket.emit('wordsToBeTranslated', AudioRecorderChangeEvent.audioData)
         console.log(AudioRecorderChangeEvent.audioData)
     }
@@ -107,23 +151,11 @@ class VoiceTranslator extends React.Component {
                         </Button>
                         <h1>{this.state.recordedBlobURL !== '' ? `${this.state.recordedBlobURL}` : `Nothing Recorded yet`} </h1>
                         <h3>{this.state.translation}</h3>
-                        {/* <h3>{this.state.returnedAudioRecordingBinaryFile}</h3> */}
+                    
 
-                        {/* <Recorder command={this.state.command} onStop={this.onStop} /> */}
+                        <AudioRecorder onRecordStart={this.analyzeVolume} onChange={(AudioRecorderChangeEvent) => this.onChange(AudioRecorderChangeEvent)} />
 
-
-                        <AudioRecorder onChange={(AudioRecorderChangeEvent) => this.onChange(AudioRecorderChangeEvent)} />
-
-                        {/* <ReactMic
-                            record={this.state.record}
-                            className="sound-wave"
-                            onStop={this.onStop}
-                            strokeColor="#000000"
-                            backgroundColor="#FF4081" /> */}
-                        < p > _____</p>
-                        {/* {/* <Button raised color="primary" onClick={this.startRecording}>
-                            Start Recording
-                        </Button> */}
+                     
                         <Button raised color="primary" onClick={this.handlePlaybackTransaltion}>
                             Play Translation
                         </Button> 
